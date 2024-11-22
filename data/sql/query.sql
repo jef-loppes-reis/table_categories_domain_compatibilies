d84b4f7e94f3f41d70f5b8f64e5ea81c05adac51
jef-loppes-reis <jefersondip25@gmail.com>
2024-11-22 11:00:28 -0300

    fix: update

--- data/sql/query.sql
+++ data/sql/query.sql
@@ -1,8 +1,17 @@
 SELECT DISTINCT ON (ml_info.item_id)
     ml_info.item_id,
     ml_info.title,
+	ml_info.status,
+	ml_info.sub_status,
+	CASE
+		WHEN ml_info.channels in ('{marketplace,mshops}', '{mshops,marketplace}') THEN 'ML/SHOPS'
+		WHEN ml_info.channels = '{mshops}' THEN 'SHOPS'
+		WHEN ml_info.channels = '{marketplace}' THEN 'ML'
+		ELSE ml_info.channels
+	END canais_venda,
     ml_info.codpro,
     ml_info.sku,
+	ml_info.storage,
     ml_info.sold_quantity,
     ml_info.has_compatibilities,
     CASE
